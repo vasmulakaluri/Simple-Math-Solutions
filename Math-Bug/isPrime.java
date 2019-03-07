3cd3cbe00c593ef94100534b4615f64652f350ec
Vasudha Mulakaluri <vasudha.m@live.com>
2019-03-07 15:26:39 -0800

    Update isPrime.java

--- Math-Bug/isPrime.java
+++ Math-Bug/isPrime.java
@@ -1,8 +1,11 @@
 public class Solution {
 	public int isPrime(int A) {
-		int upperLimit = (int)(Math.sqrt(A));
+		int upperLimit = A/2; //(int)(Math.sqrt(A));
 		for (int i = 2; i <= upperLimit; i++) {
-			if (i < A && A % i == 0) return 0;
+			if (A % i == 0 ) return 0;
+		}
+		if (A == 1 || A == 0) {
+		    return 0;
 		}
 		return 1;
 	}
